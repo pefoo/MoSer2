4e53ab804c2c72dbee1d897f7a37010457af9340
peepe <pepus.halt@gmail.com>
2018-11-20 17:01:50 +0100

    added pluginfacade (empty yet)

---
+++ src/moser2/plugin/pluginfacade.hpp
@@ -0,0 +1,15 @@
+#ifndef PLUGINFACADE_H
+#define PLUGINFACADE_H
+
+namespace moser2 {
+namespace plugin {
+///
+/// \brief TODO implement the static facade using a circular buffer
+///
+class PluginFacade {
+ public:
+ private:
+};
+}  // namespace plugin
+}  // namespace moser2
+#endif  // PLUGINFACADE_H
