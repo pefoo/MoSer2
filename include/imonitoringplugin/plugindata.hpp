3814a62d3cd389167d0a38369cd01e73fa01419c
peepe <pepus.halt@gmail.com>
2018-11-20 15:37:12 +0100

    plugindata is now printable using ToString()

--- include/imonitoringplugin/plugindata.hpp
+++ include/imonitoringplugin/plugindata.hpp
@@ -1,6 +1,7 @@
 #ifndef PLUGINDATA_H
 #define PLUGINDATA_H
 
+#include <sstream>
 #include <string>
 #include <utility>
 #include <vector>
@@ -10,6 +11,15 @@ struct PluginData {
   const std::string PluginName;
   const std::int64_t Timestamp;
   const std::vector<std::pair<std::string, std::string>> Data;
+  std::string ToString() const {
+    std::stringstream ss;
+    ss << PluginName << std::string(" [") << std::to_string(Timestamp)
+       << std::string("]: ");
+    for (const auto& d : Data) {
+      ss << d.first << std::string("(") << d.second << std::string(");");
+    }
+    return ss.str();
+  }
 };
 }
 #endif  // PLUGINDATA_H
