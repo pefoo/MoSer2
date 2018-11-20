972e5abbfd5c1f12e367071aa2e6c6132c33f3df
peepe <pepus.halt@gmail.com>
2018-11-20 15:11:15 +0100

    added the utility library

---
+++ include/utility/threading/callbacktimer.hpp
@@ -0,0 +1,51 @@
+#ifndef CALLBACKTIMER_H
+#define CALLBACKTIMER_H
+
+#include <atomic>
+#include <functional>
+#include <thread>
+
+namespace utility {
+namespace threading {
+///
+/// \brief A simple callback timer
+///
+class CallbackTimer {
+ public:
+  ///
+  /// \brief CallbackTimer
+  ///
+  CallbackTimer();
+
+  ///
+  /// \brief ~CallbackTimer
+  ///
+  ~CallbackTimer();
+
+  ///
+  /// \brief Start the timer
+  /// \param interval_ms The interval in ms
+  /// \param func The function to call
+  ///
+  void Start(int interval_ms, std::function<void(void)> func);
+
+  ///
+  /// \brief Stop the timer
+  ///
+  void Stop();
+
+  ///
+  /// \brief Get whether the timer is runner
+  /// \return True, if the timer is running
+  ///
+  bool is_running() const;
+
+ private:
+  std::atomic_bool execute;
+  std::thread thread;
+};
+
+}  // namespace threading
+}  // namespace utility
+
+#endif  // CALLBACKTIMER_H
---
+++ src/utility/threading/callbacktimer.cpp
@@ -0,0 +1,49 @@
+#include "utility/threading/callbacktimer.hpp"
+#include <exception>
+#include "easyloggingpp-9.96.5/src/easylogging++.h"
+
+namespace utility {
+namespace threading {
+
+CallbackTimer::CallbackTimer() : execute(false) {}
+
+CallbackTimer::~CallbackTimer() {
+  if (this->execute.load(std::memory_order_acquire)) {
+    this->Stop();
+  }
+}
+
+void CallbackTimer::Start(int interval_ms, std::function<void()> func) {
+  if (this->is_running()) {
+    this->Stop();
+  }
+
+  this->execute.store(true, std::memory_order_release);
+  this->thread = std::thread([=]() {
+    while (this->execute.load(std::memory_order_acquire)) {
+      try {
+        func();
+      } catch (const std::exception& e) {
+        LOG(ERROR) << "An error occurred during function execution in a "
+                      "callback timer. "
+                   << e.what();
+      }
+      std::this_thread::sleep_for(std::chrono::milliseconds(interval_ms));
+    }
+  });
+}
+
+void CallbackTimer::Stop() {
+  this->execute.store(false, std::memory_order_release);
+  if (this->thread.joinable()) {
+    this->thread.join();
+  }
+}
+
+bool CallbackTimer::is_running() const {
+  return this->execute.load(std::memory_order_acquire) &&
+         this->thread.joinable();
+}
+
+}  // namespace threading
+}  // namespace utility
