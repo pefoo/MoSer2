4519498466bde0399d2c5a9e8df526c22b2f9ff7
peepe <pepus.halt@gmail.com>
2018-11-22 13:29:46 +0100

    callback timer tests

--- test/utility/threading/callbacktimer_test.cpp
+++ test/utility/threading/callbacktimer_test.cpp
@@ -4,9 +4,9 @@
 #include "catch2/catch.hpp"
 
 ///
-/// \brief Callback timer test case
+/// \brief Callback timer basic functionality
 ///
-TEST_CASE("Callback timer", "[utility]") {
+TEST_CASE("Callback timer basic functionality", "[utility]") {
   utility::threading::CallbackTimer cb{};
   std::condition_variable cv;
   std::mutex mut;
@@ -24,4 +24,32 @@ TEST_CASE("Callback timer", "[utility]") {
   // timer is not running, wait_for should time out.
   status = cv.wait_for(lk, std::chrono::milliseconds(250));
   REQUIRE(status == std::cv_status::timeout);
+}
+
+///
+/// \brief Callback timer auto stop functionality
+///
+TEST_CASE("Callback timer auto stop", "[utility]") {
+  utility::threading::CallbackTimer* cb =
+      new utility::threading::CallbackTimer{};
+  std::condition_variable cv;
+  std::mutex mut;
+
+  cb->Start(100, [&cv]() { cv.notify_one(); });
+  delete cb;
+  std::unique_lock<std::mutex> lk(mut);
+  auto status = cv.wait_for(lk, std::chrono::milliseconds(250));
+  REQUIRE(status == std::cv_status::timeout);
+}
+
+///
+/// \brief Callback timer ignores exceptions in func
+///
+TEST_CASE("Callback timer exception in func", "[utility]") {
+  utility::threading::CallbackTimer cb{};
+
+  REQUIRE_NOTHROW([&cb]() {
+    cb.Start(10, []() { throw std::exception(); });
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  });
 }
