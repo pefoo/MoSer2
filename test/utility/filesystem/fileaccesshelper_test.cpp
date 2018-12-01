91dda9e030e8dd45e3c355678f4698cd6154c3b0
peepe <pepus.halt@gmail.com>
2018-12-01 16:47:16 +0000

    Update fileaccesshelper_test.cpp

--- test/utility/filesystem/fileaccesshelper_test.cpp
+++ test/utility/filesystem/fileaccesshelper_test.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include "catch2/catch.hpp"
 
+// Test the file exists functionality
 TEST_CASE("File access helper", "[utility]") {
   REQUIRE(utility::filesystem::FileExists("/proc/stat"));
   REQUIRE_FALSE(utility::filesystem::FileExists("foobar/slknse"));
