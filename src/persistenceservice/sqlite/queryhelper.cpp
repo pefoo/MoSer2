3e54956c8631f6a25792b6564c708c55cb144f9c
peepe <pepus.halt@gmail.com>
2019-03-29 23:48:33 +0100

    sqlite3 query builder + unit test
    supported queries:
    - create table
    - insert

--- src/persistenceservice/sqlite/queryhelper.cpp
+++ src/persistenceservice/sqlite/queryhelper.cpp
@@ -5,5 +5,55 @@ std::string persistenceservice::sqlite::QueryHelper::BuildCreateTableQuery(
     const imonitorplugin::PluginData& data) {
   std::string query = "create table if not exists " + data.plugin_name() + "(";
   for (const auto& d : data.data()) {
+    query += d.first + " " + GetSqliteType(d.second.type()) + ",";
   }
+  query += "timestamp INTEGER)";
+  return query;
+}
+
+std::string persistenceservice::sqlite::QueryHelper::BuildInsertQuery(
+    const imonitorplugin::PluginData& data) {
+  std::string query = "insert into " + data.plugin_name() + " (";
+  for (const auto& d : data.data()) {
+    query += d.first + ",";
+  }
+  query += "timestamp) values (";
+  for (auto& d : data.data()) {
+    query += GetEscapedValueAsString(d.second) + ",";
+  }
+  query += std::to_string(data.timestamp()) + ")";
+  return query;
+}
+
+std::string persistenceservice::sqlite::QueryHelper::GetSqliteType(
+    const std::type_info& type) {
+  // use operator== on some types. Cpplint gets confused about the comparison
+  // and reports old style cast errors...
+  if (type.operator==(typeid(int)) || type.operator==(typeid(int64_t)) ||
+      type.operator==(typeid(bool))) {
+    return "INTEGER";
+  } else if (type.operator==(typeid(float)) ||
+             type.operator==(typeid(double))) {
+    return "REAL";
+  } else if (type == typeid(std::string)) {
+    return "TEXT";
+  }
+
+  throw std::runtime_error("Type not supported.");
+}
+
+std::string persistenceservice::sqlite::QueryHelper::GetEscapedValueAsString(
+    utility::datastructure::Any& value) {
+  if (value.type() == typeid(int)) {
+    return std::to_string(value.get<int>());
+  } else if (value.type() == typeid(float)) {
+    return std::to_string(value.get<float>());
+  } else if (value.type() == typeid(double)) {
+    return std::to_string(value.get<double>());
+  } else if (value.type() == typeid(int64_t)) {
+    return std::to_string(value.get<int64_t>());
+  } else if (value.type() == typeid(std::string)) {
+    return "'" + value.get<std::string>() + "'";
+  }
+  throw std::runtime_error("Type not supported");
 }
--- src/persistenceservice/sqlite/queryhelper.hpp
+++ src/persistenceservice/sqlite/queryhelper.hpp
@@ -1,20 +1,36 @@
 #ifndef QUERYHELPER_H
 #define QUERYHELPER_H
 
+#include <map>
 #include <string>
 #include "imonitoringplugin/plugindata.hpp"
+#include "utility/datastructure/Any.hpp"
 
 namespace persistenceservice {
 namespace sqlite {
 ///
-/// \brief TODO
+/// \brief A helper to create sqlite queries
+/// \details Plugin names are used as table names
 ///
 class QueryHelper {
  public:
+  ///
+  /// \brief Build a create table query
+  /// \details Create table if not exists is used
+  /// \return A query to build a new table based on a plugin data object
+  ///
   static std::string BuildCreateTableQuery(const imonitorplugin::PluginData&);
+
+  ///
+  /// \brief Build a insert query
+  /// \return A query that inserts the provided data into the right table
+  ///
+  static std::string BuildInsertQuery(const imonitorplugin::PluginData&);
 
  private:
   QueryHelper() {}
+  static std::string GetSqliteType(const std::type_info& type);
+  static std::string GetEscapedValueAsString(utility::datastructure::Any&);
 };
 }  // namespace sqlite
 }  // namespace persistenceservice
