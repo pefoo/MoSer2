04fae94fb43c5246e198530344d42579a1a86509
peepe <pepus.halt@gmail.com>
2019-05-04 21:18:08 +0200

    persistence runner is using the callback timer to actually do stuff
    - uses 1 thread less
    - react faster to shutdowns

--- src/persistenceservice/persistencerunner.cpp
+++ src/persistenceservice/persistencerunner.cpp
@@ -6,33 +6,17 @@
 persistenceservice::PersistenceRunner::PersistenceRunner(
     std::unique_ptr<persistenceservice::IDataAdapter> data_adapter,
     std::function<bool(imonitorplugin::PluginData *)> source)
-    : data_adapter_(std::move(data_adapter)), source_func_(std::move(source)) {}
+    : data_adapter_(std::move(data_adapter)),
+      source_func_(std::move(source)),
+      timer_(new utility::threading::CallbackTimer{}) {}
 
 void persistenceservice::PersistenceRunner::Run(int delay) {
-  this->execute_.store(true, std::memory_order_release);
-  this->thread_ = std::thread([&]() {
-    utility::threading::CallbackTimer cb_timer{};
-    std::mutex mutex;
-    cb_timer.Start(delay, [&]() { this->cv_.notify_one(); });
-
-    while (this->execute_.load(std::memory_order_acquire)) {
-      std::unique_lock<std::mutex> lk(mutex);
-      this->cv_.wait(lk);
-      if (!this->execute_.load(std::memory_order_acquire)) break;
-
-      imonitorplugin::PluginData d;
-      while (this->source_func_(&d)) {
-        this->data_adapter_->Store(d);
-      }
+  this->timer_->Start(delay, [&]() {
+    imonitorplugin::PluginData d;
+    while (this->source_func_(&d)) {
+      this->data_adapter_->Store(d);
     }
-    cb_timer.Stop();
   });
 }
 
-void persistenceservice::PersistenceRunner::Stop() {
-  this->execute_.store(false, std::memory_order_release);
-  this->cv_.notify_one();
-  if (this->thread_.joinable()) {
-    this->thread_.join();
-  }
-}
+void persistenceservice::PersistenceRunner::Stop() { this->timer_->Stop(); }
