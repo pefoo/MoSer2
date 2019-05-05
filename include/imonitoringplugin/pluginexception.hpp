2679fe72d30594d260e26a7649454ee4434baafd
peepe <pepus.halt@gmail.com>
2019-05-05 21:29:08 +0200

    moved the plugin exception to monitoring plugin interface library

--- include/imonitoringplugin/pluginexception.hpp
+++ include/imonitoringplugin/pluginexception.hpp
@@ -1,10 +1,11 @@
 #ifndef PLUGINEXCEPTION_H
 #define PLUGINEXCEPTION_H
 
+#include <cstring>
 #include <exception>
 #include <string>
 
-namespace monitoringpluginbase {
+namespace imonitorplugin {
 
 ///
 /// \brief The plugin exception class
@@ -16,7 +17,10 @@ class PluginException : public std::exception {
   /// \param plugin_name The name of the plugin
   /// \param msg The message
   ///
-  PluginException(std::string plugin_name, std::string msg);
+  PluginException(std::string plugin_name, std::string msg)
+      : plugin_name_(std::move(plugin_name)),
+        msg_(std::move(msg)),
+        what_("Plugin: " + this->plugin_name_ + " Error: " + this->msg_) {}
 
   ///
   /// \brief ~PluginException
@@ -27,24 +31,27 @@ class PluginException : public std::exception {
   /// \brief The exception message including the plugin name
   /// \return A c style string with the exception message
   ///
-  virtual const char *what() const noexcept override;
+  virtual const char* what() const noexcept override {
+    return this->what_.c_str();
+  }
 
   ///
   /// \brief Get the plugin name
   /// \return The name of the plugin
   ///
-  std::string plugin_name() const;
+  std::string plugin_name() const { return plugin_name_; }
 
   ///
   /// \brief Get the thrown message
   /// \return The message
   ///
-  std::string msg() const;
+  std::string msg() const { return msg_; }
 
  private:
   std::string plugin_name_;
   std::string msg_;
+  std::string what_;
 };
-}  // namespace monitoringpluginbase
+}  // namespace imonitorplugin
 
 #endif  // PLUGINEXCEPTION_H
