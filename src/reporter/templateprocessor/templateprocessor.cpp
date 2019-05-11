15903d705d63e6e3a3e71d3b074332b81e4799a7
peepe <pepus.halt@gmail.com>
2019-05-11 19:50:37 +0200

    implemented token replacement

--- src/reporter/templateprocessor/templateprocessor.cpp
+++ src/reporter/templateprocessor/templateprocessor.cpp
@@ -1,1 +1,46 @@
 #include "reporter/templateprocessor/templateprocessor.hpp"
+#include <filesystem>
+#include <fstream>
+#include "utility/filesystem/fileaccesshelper.hpp"
+
+reporter::templateprocessor::TemplateProcessor::TemplateProcessor(
+    std::vector<reporter::templateprocessor::TemplateToken> tokens)
+    : tokens_(tokens) {}
+
+std::string reporter::templateprocessor::TemplateProcessor::ProcessTemplate(
+    std::string template_file, bool in_place) {
+  auto result_file = in_place
+                         ? template_file
+                         : utility::filesystem::PathCombine(
+                               {std::filesystem::temp_directory_path().string(),
+                                std::to_string(std::time(nullptr))})
+                               .string();
+
+  std::stringstream buffer;
+  std::ifstream template_stream(template_file);
+  std::string line;
+
+  while (std::getline(template_stream, line)) {
+    std::string processed_line = line;
+    for (auto& token : this->tokens_) {
+      processed_line = this->ReplaceAll(processed_line, token);
+    }
+    buffer << processed_line;
+  }
+  template_stream.close();
+
+  std::ofstream out(result_file, std::ios::trunc);
+  out << buffer.rdbuf();
+
+  return result_file;
+}
+
+std::string reporter::templateprocessor::TemplateProcessor::ReplaceAll(
+    std::string subject, TemplateToken& token) {
+  size_t pos = 0;
+  while ((pos = subject.find(token.key(), pos)) != std::string::npos) {
+    subject.replace(pos, token.key().length(), token.value());
+    pos += token.value().size();
+  }
+  return subject;
+}
--- src/reporter/templateprocessor/templateprocessor.hpp
+++ src/reporter/templateprocessor/templateprocessor.hpp
@@ -1,20 +1,38 @@
 #ifndef TEMPLATEPROCESSOR_H
 #define TEMPLATEPROCESSOR_H
 
+#include <vector>
+#include "reporter/templateprocessor/templatetoken.hpp"
+
 namespace reporter {
 namespace templateprocessor {
 
 ///
-/// \brief TODO
+/// \brief The template processor
 ///
 class TemplateProcessor {
  public:
-  /*
-   * Pass available tokens to this processor
-   * Run the processor to replace tokens in a provided file
-   */
+  ///
+  /// \brief Create a new instance
+  /// \param tokens The tokens to be used for replacement
+  ///
+  TemplateProcessor(std::vector<TemplateToken> tokens);
+
+  ///
+  /// \brief Process a template file
+  /// \param template_file The file to process
+  /// \param in_place A flag, indicating whether the result is written back to
+  /// the source file
+  /// \return The path to the result file
+  /// \details If in_place is set to false, a new file is generated. The new
+  /// file will be located in some temp path
+  ///
+  std::string ProcessTemplate(std::string template_file, bool in_place = false);
 
  private:
+  std::vector<TemplateToken> tokens_;
+
+  std::string ReplaceAll(std::string subject, TemplateToken& token);
 };
 }  // namespace templateprocessor
 }  // namespace reporter
