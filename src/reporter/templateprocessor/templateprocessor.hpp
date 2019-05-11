b9af3d763f631f19b2558113396f03c69dcffbdd
peepe <pepus.halt@gmail.com>
2019-05-11 19:05:42 +0200

    added missing namespaces

--- src/reporter/templateprocessor/templateprocessor.hpp
+++ src/reporter/templateprocessor/templateprocessor.hpp
@@ -1,18 +1,22 @@
 #ifndef TEMPLATEPROCESSOR_H
 #define TEMPLATEPROCESSOR_H
 
+namespace reporter {
+namespace templateprocessor {
+
 ///
 /// \brief TODO
 ///
-class TemplateProcessor
-{
-public:
+class TemplateProcessor {
+ public:
   /*
    * Pass available tokens to this processor
    * Run the processor to replace tokens in a provided file
    */
 
-private:
+ private:
 };
+}  // namespace templateprocessor
+}  // namespace reporter
 
-#endif //TEMPLATEPROCESSOR_H
+#endif  // TEMPLATEPROCESSOR_H
