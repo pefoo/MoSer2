a770b6c3f3a3c1f98b26cff770727afe819aa3f1
peepe <pepus.halt@gmail.com>
2019-05-14 13:14:14 +0200

    get settings and configure logger in reporter app

--- src/reporter/main.cpp
+++ src/reporter/main.cpp
@@ -1,6 +1,21 @@
+#include <memory>
 #include <string>
+#include "constants/settings/settingsidentifier.hpp"
+#include "core/appsetup.hpp"
+#include "core/settings/settingshelper.hpp"
 #include "easyloggingpp-9.96.5/src/easylogging++.h"
+#include "persistenceservice/adapterfactory.hpp"
+#include "persistenceservice/sqlite/sqlitesettings.hpp"
 
 INITIALIZE_EASYLOGGINGPP
 
-int main() { return 0; }
+int main() {
+  core::ConfigureLogger();
+  auto settings = core::settings::GetApplicationSettings();
+  auto adapter_factory = std::make_shared<persistenceservice::AdapterFactory>(
+      std::make_unique<persistenceservice::sqlite::SqliteSettings>(
+          settings->GetValue(constants::settings::SqliteDatabaseFile())));
+  auto adapter = adapter_factory->CreateAdapter();
+
+  return 0;
+}
