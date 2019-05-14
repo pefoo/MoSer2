e454e84dbf42b69fbffb409c66f03592f63265d7
peepe <pepus.halt@gmail.com>
2019-05-14 17:21:48 +0200

    enabling dynamic tokens
    - new constants for factory names

--- include/imonitoringplugin/constants.hpp
+++ include/imonitoringplugin/constants.hpp
@@ -1,4 +1,13 @@
 #ifndef CONSTANTS_HPP
 #define CONSTANTS_HPP
 
+namespace imonitoringplugin {
+
+static constexpr char kMonitoringPluginConstructor[] = "create";
+static constexpr char kMonitoringPluginDestructor[] = "destroy";
+static constexpr char kDataProcessorConstructor[] = "create_processors";
+static constexpr char kDataProcessorDestructor[] = "destroy_processors";
+
+} // namespace imonitoringplugin
+
 #endif // CONSTANTS_HPP
