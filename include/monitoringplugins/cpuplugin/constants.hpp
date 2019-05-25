91a8bb93167623db2892ddd549776020c248ef91
peepe <pepus.halt@gmail.com>
2019-05-25 21:13:31 +0200

    cpu data processor
    - time series chart
    - average value

--- include/monitoringplugins/cpuplugin/constants.hpp
+++ include/monitoringplugins/cpuplugin/constants.hpp
@@ -5,6 +5,10 @@ namespace monitoringplugins {
 namespace cpuplugin {
 namespace constants {
 
+static constexpr char kGpArgFileName[] = "file_name";
+static constexpr char kGpArgYLabel[] = "y_label";
+static constexpr char kGpArgYSeriesCount[] = "y_series_count";
+static constexpr char kGpScriptName[] = "cpu_charts.gp";
 static constexpr char kPluginName[] = "CpuPlugin";
 
 }  // namespace constants
