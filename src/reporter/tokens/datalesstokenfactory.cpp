5e92fcd435f66ef5aa798f329261ae95414edb1c
peepe <pepus.halt@gmail.com>
2019-05-27 17:43:13 +0200

    moved the macros to create tokens

--- src/reporter/tokens/datalesstokenfactory.cpp
+++ src/reporter/tokens/datalesstokenfactory.cpp
@@ -1,19 +1,20 @@
 #include "datalesstokenfactory.hpp"
 #include <sys/utsname.h>
-
-#define CREATETOKEN(token_name, body)               \
-  ::reporter::templateprocessor::TemplateToken {    \
-    token_name, [sysinfo]() -> std::string { body } \
-  }
+#include <vector>
 
 std::vector<reporter::templateprocessor::TemplateToken>
 reporter::tokens::GetDatalessTokens() {
   struct utsname sysinfo;
   uname(&sysinfo);
   return std::vector<reporter::templateprocessor::TemplateToken>{
-      CREATETOKEN("%%HOSTNAME%%", { return sysinfo.nodename; }),
-      CREATETOKEN("%%KERNEL_VERSION%%", { return sysinfo.release; }),
-      CREATETOKEN("%%SYSTEM_NAME%%", { return sysinfo.sysname; }),
-      CREATETOKEN("%%MACHINE_ARCH%%", { return sysinfo.machine; }),
-      CREATETOKEN("%%DOMAIN_NAME%%", { return sysinfo.domainname; })};
+      CREATE_TOKEN_CAPTURE("%%HOSTNAME%%", sysinfo,
+                           { return sysinfo.nodename; }),
+      CREATE_TOKEN_CAPTURE("%%KERNEL_VERSION%%", sysinfo,
+                           { return sysinfo.release; }),
+      CREATE_TOKEN_CAPTURE("%%SYSTEM_NAME%%", sysinfo,
+                           { return sysinfo.sysname; }),
+      CREATE_TOKEN_CAPTURE("%%MACHINE_ARCH%%", sysinfo,
+                           { return sysinfo.machine; }),
+      CREATE_TOKEN_CAPTURE("%%DOMAIN_NAME%%", sysinfo,
+                           { return sysinfo.domainname; })};
 }
--- src/reporter/tokens/datalesstokenfactory.hpp
+++ src/reporter/tokens/datalesstokenfactory.hpp
@@ -2,10 +2,34 @@
 #define DATALESSTOKENFACTORY_H
 
 #include <vector>
+#include <string>
 #include "reporter/templateprocessor/templatetoken.hpp"
 
 namespace reporter {
 namespace tokens {
+
+///
+/// \brief Create a token
+/// \details Creates a token with default signature
+/// \param token_name The name of the token
+/// \param body The body of the lambda expression for the token
+///
+#define CREATE_TOKEN(token_name, body)           \
+  ::reporter::templateprocessor::TemplateToken { \
+    token_name, []() -> std::string { body }     \
+  }
+
+///
+/// \brief Create a token
+/// \details Creates a token with default signature
+/// \param token_name The name of the token
+/// \param capture A capture expression for the lambda
+/// \param body The body of the lambda expression for the token
+///
+#define CREATE_TOKEN_CAPTURE(token_name, capture, body) \
+  ::reporter::templateprocessor::TemplateToken {        \
+    token_name, [capture]() -> std::string { body }     \
+  }
 
 ///
 /// \brief Create tokens that do not require any data input
