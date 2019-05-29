f30991665cb97ed599fb47335ae0bb3d6b33bba4
peepe <pepus.halt@gmail.com>
2019-05-29 14:37:46 +0200

    catch exception during token evaluation

--- src/reporter/templateprocessor/templatetoken.cpp
+++ src/reporter/templateprocessor/templatetoken.cpp
@@ -1,5 +1,6 @@
 #include "reporter/templateprocessor/templatetoken.hpp"
 #include <string>
+#include "easyloggingpp-9.96.5/src/easylogging++.h"
 
 reporter::templateprocessor::TemplateToken::TemplateToken(
     std::string key, std::function<std::string()> value_func, bool cache)
@@ -13,5 +14,11 @@ std::string reporter::templateprocessor::TemplateToken::value() {
   if (this->cache_ && !this->value_.empty()) {
     return this->value_;
   }
-  return this->value_ = this->value_func_();
+  try {
+    return this->value_ = this->value_func_();
+  } catch (std::exception &e) {
+    LOG(ERROR) << "Failed to get value of token " << this->key()
+               << " Error: " << e.what();
+    return "";
+  }
 }
