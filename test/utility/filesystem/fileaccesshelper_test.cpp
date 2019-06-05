278bdc7b0190348445a5f7c7a227dc2ba4f8c65d
peepe <pepus.halt@gmail.com>
2019-06-05 16:41:28 +0200

    fixed fileaccesshelper + unit test

--- test/utility/filesystem/fileaccesshelper_test.cpp
+++ test/utility/filesystem/fileaccesshelper_test.cpp
@@ -7,3 +7,17 @@ TEST_CASE("Path combine", "[utility]") {
       utility::filesystem::PathCombine({"/", "foobar", "/", "bar", "foo.txt"});
   REQUIRE(path == "/foobar/bar/foo.txt");
 }
+
+TEST_CASE("Current path", "[utility]") {
+  auto path = utility::filesystem::GetCurrentExecutablePath();
+  REQUIRE(path == LIBRARY_DIR);
+}
+
+TEST_CASE("Make absolute path", "[utility]") {
+  auto untouched =
+      utility::filesystem::MakeAbsolutePathFromExecutable("/.foobar");
+  REQUIRE(untouched == "/.foobar");
+
+  auto path = utility::filesystem::MakeAbsolutePathFromExecutable("foobar");
+  REQUIRE(path == std::string(LIBRARY_DIR) + "/foobar");
+}
