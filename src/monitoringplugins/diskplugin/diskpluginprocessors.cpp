0bb0d812a3a53ad466faa7c5f1eceb19c16e685f
peepe <pepus.halt@gmail.com>
2019-06-29 16:02:47 +0200

    added the disk usage token

--- src/monitoringplugins/diskplugin/diskpluginprocessors.cpp
+++ src/monitoringplugins/diskplugin/diskpluginprocessors.cpp
@@ -1,7 +1,9 @@
 #include "monitoringplugins/diskplugin/diskpluginprocessors.hpp"
 #include <sys/statvfs.h>
+#include <cmath>
 #include <fstream>
 #include <memory>
+#include <sstream>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -30,11 +32,13 @@ monitoringplugins::diskplugin::CreateProcessors() {
       processors{};
 
   if (settings) {
-    auto mount_points = GetMountPoints();
     auto devices = std::stringstream{settings->GetValue("Devices", "")};
     std::string device;
     std::vector<std::string> device_list;
     while (std::getline(devices, device, ';')) {
+      device_list.push_back(device);
+    }
+    for (const auto& device : device_list) {
       processors.push_back(
           std::make_shared<monitoringpluginbase::PluginDataProcessor>(
               "%%DISK_" + device + "_TIME_SERIES_DATA%%",
@@ -54,11 +58,46 @@ monitoringplugins::diskplugin::CreateProcessors() {
                     monitoringplugins::diskplugin::constants::kGpArgFileName,
                     params, ";", record_filter);
               }));
-      if (mount_points.count("/dev/" + device) != 0) {
-        auto u = GetDiskTotal(mount_points["/dev/" + device]);
-        int i = 4;
-      }
     }
+    processors.push_back(
+        std::make_shared<monitoringpluginbase::PluginDataProcessor>(
+            "%%DISK_USAGE%%",
+            [device_list](
+                [[gnu::unused]] std::vector<imonitorplugin::PluginData> records)
+                -> std::string {
+              auto mount_points = GetMountPoints();
+              std::stringstream out{};
+              out << "<table style=\"width:100%\"><tr><th "
+                     "style=\"width:15%\">Device</th><th "
+                     "style=\"width:35%\">Mount</th><th "
+                     "style=\"width:50%\">Used</th></tr>";
+              std::string device;
+              for (const auto& device : device_list) {
+                if (mount_points.count("/dev/" + device) != 0) {
+                  auto total =
+                      GetDiskTotal(mount_points["/dev/" + device]) / pow(2, 30);
+                  auto available =
+                      GetDiskAvailable(mount_points["/dev/" + device]) /
+                      pow(2, 30);
+                  auto usage = (1 - available / total) * 100;
+
+                  out << "<tr><td>" << device << "</td><td>"
+                      << mount_points["/dev/" + device]
+                      << "</td><td><div style=\"border:1px solid black; "
+                         "height:1.3em\"><div style=\"position:absolute; "
+                         "display:table; "
+                         "left:70%\">"
+                      << round(total - available) << "/" << round(total)
+                      << " [Gb]</div> <div style=\"background-color:"
+                      << (usage > 70 ? (usage > 90 ? "red" : "yellow")
+                                     : "green")
+                      << "; height:100%; width:" << usage
+                      << "%\"></div></div></td></tr>";
+                }
+              }
+              out << "</table>";
+              return out.str();
+            }));
   }
 
   return processors;
