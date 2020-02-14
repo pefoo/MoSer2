9bf12ce1cd09a1848d92463fc3cb0877192f04c5
peepe <pepus.halt@gmail.com>
2020-02-14 22:02:42 +0100

    added network usage statistics

--- README.md
+++ README.md
@@ -174,6 +174,7 @@ Eventually, the result of the replacement is send as an email.
 |%%DISK_USAGE%%|A table that shows the disk usage of all configured devices.|
 |**Network plugin:**||
 |%%NETWORK_TIMES_SERIES_DATA%%|A base 64 encoded time series chart of the network usage.|
+|%%NETWORK_USAGE_STATS%%|Network usage statistics.|
 |**Logwatcher plugin:**||
 |%%LOG_TABLE_{tag}%%|A html table that displays the log messages for the given tag.|
 |%%LOG_CHART_{tag}%%|A chart that displays a histogram for the events with a given tag.|
--- src/monitoringplugins/networkplugin/networkpluginprocessors.cpp
+++ src/monitoringplugins/networkplugin/networkpluginprocessors.cpp
@@ -1,8 +1,19 @@
 #include "monitoringplugins/networkplugin/networkpluginprocessors.hpp"
+#include <cmath>
 #include "dataprocessorhelper/gnuplot/gnuplotparameterdict.hpp"
 #include "dataprocessorhelper/gnuplot/gnuplotwrapper.hpp"
+#include "dataprocessorhelper/tablecalchelper.hpp"
 #include "monitoringplugins/networkplugin/constants.hpp"
 #include "utility/datastructure/table.hpp"
+
+static constexpr char kTableStart[] = "<table class=default>";
+static constexpr char kTableEnd[] = "</table>";
+static constexpr char kTableRowStart[] = "<tr class=default>";
+static constexpr char kTableRowEnd[] = "</tr>";
+static constexpr char kTableHeaderStart[] = "<th class=default>";
+static constexpr char kTableHeaderEnd[] = "</th>";
+static constexpr char kTableColumnStart[] = "<td class=default>";
+static constexpr char kTableColumnEnd[] = "</td>";
 
 monitoringpluginbase::PluginDataProcessorCollection::ProcessorVector
 monitoringplugins::networkplugin::CreateProcessors() {
@@ -17,5 +28,50 @@ monitoringplugins::networkplugin::CreateProcessors() {
                  monitoringplugins::networkplugin::constants::
                      kPluginGpScirptName,
                  data, params);
-           })}};
+           })},
+      {// Network usage statistics
+       std::make_shared<monitoringpluginbase::PluginDataProcessor>(
+           "%%NETWORK_USAGE_STATS%%",
+           [](utility::datastructure::Table data) -> std::string {
+             if (data.MaxSize() == 0) {
+               return "";
+             }
+
+             std::stringstream table{};
+             // build the table start with columns: Core, Avg, Min, Max
+             table << kTableStart << kTableRowStart << kTableHeaderStart
+                   << "Type" << kTableHeaderEnd << kTableHeaderStart
+                   << "Avg [kB]" << kTableHeaderEnd << kTableHeaderStart
+                   << "Min [kB]" << kTableHeaderEnd << kTableHeaderStart
+                   << "Max [kB]" << kTableHeaderEnd << kTableHeaderStart
+                   << "&#x3C3; [kB]"  // sigma
+                   << kTableHeaderEnd << kTableRowEnd;
+
+             for (auto type : {"rx", "tx"}) {
+               auto filter =
+                   dataprocessorhelper::Filter::GetInclusiveFilter({type});
+               // nice try clang format...
+               // clang-format off
+               // Calculate and convert the stats to kibibyte
+               auto avg = std::round(
+                     dataprocessorhelper::Avg<int>(data, filter) * 10 * 0.000976562) / 10;
+               auto min = std::round(
+                     dataprocessorhelper::Min<int>(data, filter) * 10 * 0.000976562) / 10;
+               auto max = std::round(
+                     dataprocessorhelper::Max<int>(data, filter) * 10 * 0.000976562) / 10;
+               auto std_dev = std::round(
+                     dataprocessorhelper::StdDev<int>(data, filter) * 10 * 0.000976562) / 10;
+               // clang-format on
+
+               table << kTableRowStart << kTableColumnStart << type
+                     << kTableColumnEnd << kTableColumnStart << avg
+                     << kTableColumnEnd << kTableColumnStart << min
+                     << kTableColumnEnd << kTableColumnStart << max
+                     << kTableColumnEnd << kTableColumnStart << std_dev
+                     << kTableColumnEnd << kTableRowEnd;
+             }
+             table << kTableEnd;
+             return table.str();
+           })},
+  };
 }
