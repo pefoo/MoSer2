86bb11bea738bd9258f449ea6fd9827edc18d772
peepe <pepus.halt@gmail.com>
2020-04-25 15:52:57 +0200

    double check existance of folders in /proc
    - the directory iterator may return ids of processes that are no longer available

--- src/reporter/helper/processinformationhelper.cpp
+++ src/reporter/helper/processinformationhelper.cpp
@@ -11,6 +11,15 @@ reporter::helper::GetProcessInformation() {
   for (const auto& entry : std::filesystem::directory_iterator("/proc")) {
     if (entry.is_directory() &&
         StringRgxGrep(entry.path(), R"(^/proc/(\d+)$)", &sm)) {
+      /* Double check existence. From directory_iterator docs:
+       * "If a file or a directory is deleted or added to the directory tree
+       * after the directory iterator has been created, it is unspecified
+       * whether the change would be observed through the iterator."
+       *
+       * Thus, the iterator might (and actually did in the past) return ids of
+       * processes that do not exist anymore.
+       */
+      if (!std::filesystem::exists(entry)) continue;
       procs.push_back(
           reporter::datamodel::ProcessInformation{std::stoul(sm[1])});
     }
